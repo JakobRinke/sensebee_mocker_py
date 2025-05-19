cd22a975e4649113428a856bee6d433fceaf8858
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-05-19 15:05:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import py_sensbee.sensor as sensor
 import time
 
-test_id = "80071a71-9328-4936-8e06-dc134ccd2184"
-s = sensor.Sensor(test_id, write_api_key="5e21cd38-caaf-4427-9b04-3b357fba3c36", read_api_key="4a54a59e-abfc-4a26-a21f-5a9695c6cc80")
+test_id = "c359f1e8-0493-4cd6-945b-db690760a22c"
+s = sensor.Sensor(test_id, read_api_key="6849fe40-95a8-447e-bf6f-9e08b35c3f6c")
 
 
 # s.upload_data([{
 #     "temperature": 23.0,
 # }])
 
-d = s.get_data(limit=1)
+d = s.get_data(limit=5)
 print(d)
