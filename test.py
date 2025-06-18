5d7410f94c0d98c2497accdfc18c6df19915c44c
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-06-18 08:23:10 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,5 @@ test_id = "64c3f9f9-06e5-451e-b472-e4dee929c59e"
 s = sensor.Sensor(test_id, read_api_key="3eba757e-39b5-455a-9be1-338706a80f41")
 
 
-# s.upload_data([{
-#     "temperature": 23.0,
-# }])
-
-d = s.get_data(limit=5)
+d = s.get_data(limit=10)
 print(d)
