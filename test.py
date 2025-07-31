aa3286b43fb064ac525247f30db06241a51fecba
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2025-07-31 10:46:37 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,5 @@ import time
 
 test_id = "64c3f9f9-06e5-451e-b472-e4dee929c59e"
 s = sensor.Sensor(test_id, read_api_key="3eba757e-39b5-455a-9be1-338706a80f41")
-
-
 d = s.get_data(limit=100, )
 print(d)
